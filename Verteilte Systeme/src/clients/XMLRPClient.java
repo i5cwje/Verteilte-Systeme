ca6e87a54e1e80cb66630bf3325f979aaca3b159
Can <ckedik.ck@googlemail.com>
2014-11-24 21:16:05 +0100

    Erste Skizze von dem ganzen XMLRPC Kram noch nicht lauffähig aber sieht im moment eigt. ganz gut aus.

---
+++ Verteilte Systeme/src/clients/XMLRPClient.java
@@ -0,0 +1,44 @@
+package clients;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+
+public class XMLRPClient {
+	static float lastTime=0;
+	static float sendTime=0;
+	static Object params[];
+	static String result; 
+	
+	public static void main(String[] args) throws MalformedURLException, XmlRpcException {
+
+		XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+
+		config.setServerURL(new URL("http://127.0.0.1:8080/xmlrpc")); //Diese Zeile wirft die MalformedURL exception
+		XmlRpcClient client = new XmlRpcClient();
+		client.setConfig(config);
+		
+		while (true) {
+			sendTime += getDelta();
+			if (sendTime >= 104.1f) {
+				sendTime = 0;
+				result = (String)client.execute("BananenSindLecker.updateMyFuckingTable",params); // Das hier führt scheinbar den RPC aus.
+			}
+			System.out.println("Du wolltest ne Antwort hier hast du sie du Hurensohn: " + result);
+		}
+	}	
+
+	public static long getTime() {
+		return (System.currentTimeMillis());
+	}
+
+	public static long getDelta() {
+		long time = getTime();
+		long delta = (long) (time - lastTime);
+		lastTime = time;
+		return delta;
+	}
+}
